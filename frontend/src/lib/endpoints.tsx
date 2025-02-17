0499112acca5c6a1e9ca0eb86ade2d89c70c2c82
nezahakim <mugwanezahakimu@gmail.com>
2025-02-18 00:07:49 +0200

    v.0.0.1

--- frontend/src/lib/endpoints.tsx
+++ frontend/src/lib/endpoints.tsx
@@ -1,4 +1,4 @@
 export const AEP = "http://localhost:3001"
-export const API_BASE = "http://localhost:3001"
-// export const API_BASE = 'https://notifymusic.onrender.com'
+// export const API_BASE = "http://localhost:3001"
+export const API_BASE = 'https://notifymusic.onrender.com'
 export const API_BASE_SOCKET = 'https://notifymusic.onrender.com'
