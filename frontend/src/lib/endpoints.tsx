147ccce4c26e02a328bafed65e9772702a065cef
nezahakim <mugwanezahakimu@gmail.com>
2025-02-20 15:24:13 +0200

    new

--- frontend/src/lib/endpoints.tsx
+++ frontend/src/lib/endpoints.tsx
@@ -1,3 +1,3 @@
 // export const API_BASE = "http://localhost:3001"
-// export const API_BASE = 'https://notifymusic.onrender.com'
-export const API_BASE = 'https://notifymusic-backend.vercel.app'
+export const API_BASE = 'https://notifymusic.onrender.com'
+// export const API_BASE = 'https://notifymusic-backend.vercel.app'
